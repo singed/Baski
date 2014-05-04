6a1fd30dc56b9c31f34c1c2325f99e82a9885cf0
Singed <parfenchuks@gmail.com>
2014-05-04 20:24:16 +0300

    Fist commit Baski

---
+++ Baski/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baski.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Defaul/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Roster()
+        {
+            return View();
+        }
+    }
+}
---
+++ Baski/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Baski.MvcApplication" Language="C#" %>
